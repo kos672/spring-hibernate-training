59e7fcd78b270fe6363cef60e928a834ce2e8ceb
Konstantin Kuzmin <kocc95@mail.ru>
2018-04-09 23:01:25 +0200

    configuration refactoring

--- src/main/java/com/kuzmin/train/training/configuration/MvcConfiguration.java
+++ src/main/java/com/kuzmin/train/training/configuration/MvcConfiguration.java
@@ -2,13 +2,27 @@ package com.kuzmin.train.training.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.kuzmin.train.training.endpoint") // place where are MVC Controllers
-public class MvcConfiguration {
+public class MvcConfiguration implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp();
+    }
 }
--- src/main/java/com/kuzmin/train/training/configuration/SwaggerConfig.java
+++ src/main/java/com/kuzmin/train/training/configuration/SwaggerConfig.java
@@ -13,24 +13,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-@EnableWebMvc
+
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.jsp();
-    }
+public class SwaggerConfig  {
 
     @Bean
     public Docket api() {
